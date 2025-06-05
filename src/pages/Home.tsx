553d3d1f30bc9a68cef5816a6b896261d09318ab
Eric Champlin <eric.champlin@ramseysolutions.com>
2025-06-05 14:54:10 -0500

    refreshed testimonials

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "./Home.css";
+// @ts-ignore
 import NavBar from "../components/NavBar.tsx";
+// @ts-ignore
 import Footer from "../components/Footer.tsx";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Link } from "react-router-dom";
+// @ts-ignore
 import DanceClasses from "../Hooks/useInViewDance.tsx";
+// @ts-ignore
 import MusicClasses from "../Hooks/useInViewMusic.tsx";
+// @ts-ignore
 import FeaturedClasses from "../Hooks/useInViewFeatured.tsx";
+// @ts-ignore
 import Testimonials from "../Hooks/useInViewTestimonials.tsx";
+// @ts-ignore
 import MeetOurTeam from "../Hooks/useInViewTeam.tsx";
+// @ts-ignore
 import WhyUs from "../Hooks/useInViewWhyUs.tsx";
+// @ts-ignore
 import NextSteps from "../Hooks/useInViewNextSteps.tsx";
 
 const Home = () => {
