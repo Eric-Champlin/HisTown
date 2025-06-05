e66d849057a2fc67e91e4a727effede6fded3a96
Eric Champlin <eric.champlin@ramseysolutions.com>
2025-06-05 16:48:44 -0500

    fixed animation issues

--- src/Hooks/useInViewDance.tsx
+++ src/Hooks/useInViewDance.tsx
@@ -8,32 +8,32 @@ const DanceClasses = () => {
   });
 
   return (
-    <section className="music-lessons">
-      <div className="music-lessons-inner">
-        <div
-          className={`music-image-container slide-left ${danceVisible ? "show" : ""}`}
-          ref={danceRef}
-        >
-          <img src="https://i.imgur.com/HLP9IVj.jpeg" alt="Dance Classes" />
-        </div>
-        <div className="music-text-container">
+      <section className="music-lessons">
+        <div className="music-lessons-inner" ref={danceRef}>
           <div
-            className={`class-box slide-right ${danceVisible ? "show" : ""}`}
+              className={`music-image-container slide-left ${danceVisible ? "show" : ""}`}
           >
-            <h1>DANCE CLASSES</h1>
-            <p>
-              Offering dance lessons in acro, ballet, barre fitness,
-              contemporary, creative movement, foundations, hip‑hop, jazz,
-              modern, musical theater, and tap!
-            </p>
-            <Link to="/classes/dance" className="learn-more">
-              LEARN MORE
-            </Link>
+            <img src="https://i.imgur.com/HLP9IVj.jpeg" alt="Dance Classes" />
+          </div>
+          <div className="music-text-container">
+            <div
+                className={`class-box slide-right ${danceVisible ? "show" : ""}`}
+            >
+              <h1>DANCE CLASSES</h1>
+              <p>
+                Offering dance lessons in acro, ballet, barre fitness,
+                contemporary, creative movement, foundations, hip‑hop, jazz,
+                modern, musical theater, and tap!
+              </p>
+              <Link to="/classes/dance" className="learn-more">
+                LEARN MORE
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
+
 };
 
 export default DanceClasses;
