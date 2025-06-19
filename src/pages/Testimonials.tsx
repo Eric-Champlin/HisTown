aff5afb743a22c6bb503004f77a6116d9ff3bb3f
Eric Champlin <eric.champlin@ramseysolutions.com>
2025-06-19 16:23:32 -0500

    fixed testimonials

--- src/pages/Testimonials.tsx
+++ src/pages/Testimonials.tsx
@@ -3,19 +3,19 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import NextSteps from "../Hooks/useInViewNextSteps";
 import BannerCarousel from "../components/BannerCarousel";
-import Testimonials from "../Hooks/useInViewTestimonials.tsx";
+import TestimonialsPage from "../Hooks/useInViewTestimonialsPage";
+import "./Testimonials.css";
 
-const TestimonialsPage = () => {
+const TestimonialsPageContainer = () => {
     return (
         <div className="testimonials-page">
             <NavBar />
             <BannerCarousel />
-
-
+            <TestimonialsPage />
             <NextSteps />
             <Footer />
         </div>
     );
 };
 
-export default TestimonialsPage;
+export default TestimonialsPageContainer;
